5148dd7daec4ebbc8a318d63be2e5c3fb5cd8342
fasnimo <fasnimo@gmail.com>
2020-12-14 21:32:26 -0600

    add

--- skater-client/src/components/SkateShow.js
+++ skater-client/src/components/SkateShow.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from "react-redux"
-
 import "bootstrap/dist/css/bootstrap.min.css"
 import Card from "react-bootstrap/Card"
 import Container from "react-bootstrap/Container"
